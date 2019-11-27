e2cf1b501a909a962bd896ecb6de59155ff3130f
zhangxun <zhangxun@autohome.com.cn>
2019-11-28 00:36:19 +0800

    feat: update sql

--- shop.sql
+++ shop.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80016
  File Encoding         : 65001
 
- Date: 26/11/2019 07:25:34
+ Date: 28/11/2019 00:35:59
 */
 
 SET NAMES utf8mb4;
@@ -26,7 +26,7 @@ CREATE TABLE `admin`  (
   `admin` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of admin
@@ -88,6 +88,52 @@ INSERT INTO `goods` VALUES (11, '//img.alicdn.com/bao/uploaded/i1/106802831/TB2b
 INSERT INTO `goods` VALUES (12, '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 0000000001, 0000000236, 0000000599, 0000000366, 4, '//img.alicdn.com/imgextra/i1/2010921214/O1CN01z7GoGc1Kq3OhP1T6J_!!2010921214-0-pixelsss.jpg_60x60q90.jpg;//img.alicdn.com/imgextra/i4/2010921214/O1CN01AihXRJ1Kq3O9k60sl_!!2010921214.jpg_60x60q90.jpg;//img.alicdn.com/imgextra/i1/2010921214/O1CN01ib1mns1Kq3O8gq5Kw_!!2010921214.jpg_60x60q90.jpg;//img.alicdn.com/imgextra/i3/2010921214/O1CN01sX17ja1Kq3O9k6HVG_!!2010921214.jpg_60x60q90.jpg;//img.alicdn.com/imgextra/i4/2010921214/O1CN01vXNy4X1Kq3O3BdrZ6_!!2010921214.jpg_60x60q90.jpg', '黑,白,红,绿,蓝,紫', 568, 'S,M,L,XL,2XL,3XL');
 
 -- ----------------------------
+-- Table structure for goods_car
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_car`;
+CREATE TABLE `goods_car`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `goodsid` int(11) NULL DEFAULT NULL,
+  `userid` int(11) NULL DEFAULT NULL,
+  `count` int(11) NULL DEFAULT NULL,
+  `color` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `size` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `type` int(255) NULL DEFAULT NULL,
+  `ordercode` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `imgurl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `price` int(10) NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `date` datetime(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for goods_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_comment`;
+CREATE TABLE `goods_comment`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `goodsid` int(11) NULL DEFAULT NULL,
+  `userid` int(11) NULL DEFAULT NULL,
+  `date` datetime(6) NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `replyuserid` int(11) NULL DEFAULT NULL,
+  `replyusername` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `txt` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of goods_comment
+-- ----------------------------
+INSERT INTO `goods_comment` VALUES (29, 1, 5, '2019-11-27 13:36:41.319000', 'b@163.com', NULL, NULL, '666');
+INSERT INTO `goods_comment` VALUES (30, 1, 5, '2019-11-27 14:49:02.392000', 'b@163.com', NULL, NULL, '6666/t/tb<br>@163.com说：6666/n');
+INSERT INTO `goods_comment` VALUES (34, 2, 5, '2019-11-27 15:39:12.005000', 'b@163.com', NULL, NULL, '666<br>b@163.com 回复 b@163.com 说：888<br>b@163.com 回复 b@163.com 说：999<br>b@163.com 回复 b@163.com 说：ok');
+INSERT INTO `goods_comment` VALUES (35, 1, 5, '2019-11-27 16:06:28.796000', 'b@163.com', NULL, NULL, 'aa++\n\n\naaaa\ndd');
+INSERT INTO `goods_comment` VALUES (36, 1, 5, '2019-11-27 16:16:47.266000', 'b@163.com', NULL, NULL, '<span class=\"comments__item__content\">aa++\n\n\naaaa\ndd</span>');
+
+-- ----------------------------
 -- Table structure for goods_order
 -- ----------------------------
 DROP TABLE IF EXISTS `goods_order`;
@@ -104,17 +150,29 @@ CREATE TABLE `goods_order`  (
   `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `price` int(10) NULL DEFAULT NULL,
   `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `date` varbinary(255) NULL DEFAULT NULL,
+  `date` datetime(6) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of goods_order
 -- ----------------------------
-INSERT INTO `goods_order` VALUES (5, 1, 5, 1, '黑', 'S', 1, 'a6eb1e0544c4496d8d9daa40a4baa5dc', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 239, 'b@163.com', 0x32303139C4EA3131D4C23236C8D537CAB132B7D63439C3EB);
-INSERT INTO `goods_order` VALUES (6, 1, 5, 1, '黑', 'S', 1, 'c6128051c975429fadd553c3afb929a1', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 239, 'b@163.com', 0x32303139C4EA3131D4C23236C8D537CAB132B7D63439C3EB);
-INSERT INTO `goods_order` VALUES (7, 1, 5, 1, '黑', 'S', 3, '73c855c44c3745ceb5154b46b1fd1577', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 239, 'b@163.com', 0x32303139C4EA3131D4C23236C8D537CAB132B7D63439C3EB);
-INSERT INTO `goods_order` VALUES (8, 1, 5, 1, '黑', 'S', 2, '3a6d8f141fd2450d9c1c356f46fdb2fc', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 239, 'b@163.com', 0x32303139C4EA3131D4C23236C8D537CAB132B7D63439C3EB);
+INSERT INTO `goods_order` VALUES (13, 1, 5, 1, '黑', 'S', -1, '4d28ce63340344dd993254d2ba7cae9c', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 239, 'b@163.com', '2019-11-26 15:51:12.145000');
+INSERT INTO `goods_order` VALUES (14, 2, 5, 1, '黑', 'S', 1, '323fcc07354244fa8971262803fb25da', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-26 22:57:45.338000');
+INSERT INTO `goods_order` VALUES (15, 2, 5, 1, '黑', 'S', 1, 'd2027f9cdf004b2eb2e11a9f9476cf76', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-26 23:15:58.194000');
+INSERT INTO `goods_order` VALUES (16, 2, 5, 1, '黑', 'S', 1, '648106ccc34f42208b507c2cb968ca3e', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-26 23:20:22.232000');
+INSERT INTO `goods_order` VALUES (17, 2, 5, 1, '黑', 'S', 1, 'ff7d58fca9f7474ab6b162ef441d8386', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-26 23:21:22.217000');
+INSERT INTO `goods_order` VALUES (18, 2, 5, 1, '黑', 'S', 1, '62707e9b39134a01aef80506a5dcfcc1', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-26 23:22:21.540000');
+INSERT INTO `goods_order` VALUES (19, 2, 5, 1, '黑', 'S', 1, 'e33e85c958694374808535aa2fcd3bef', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-26 23:23:47.015000');
+INSERT INTO `goods_order` VALUES (20, 2, 5, 1, '黑', 'S', 1, '2852ca007903443984eadbfb242d8b50', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-26 23:24:49.859000');
+INSERT INTO `goods_order` VALUES (21, 2, 5, 1, '黑', 'S', 1, 'b0fcf9f07b27449fbc67a508cc4c276c', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-27 12:42:37.465000');
+INSERT INTO `goods_order` VALUES (22, 2, 5, 1, '黑', 'S', 1, 'fcf16ca85df34119b815562c90cec0bb', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-27 12:42:37.465000');
+INSERT INTO `goods_order` VALUES (23, 2, 5, 1, '黑', 'S', 1, '7ad5c70b788e40c0886d72d6b6749581', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-27 12:42:37.465000');
+INSERT INTO `goods_order` VALUES (24, 2, 5, 1, '黑', 'S', 1, 'e679a853f2254ee894596c3d565db470', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-27 12:42:37.465000');
+INSERT INTO `goods_order` VALUES (25, 2, 5, 4, '黑', 'S', 1, '7fd32ea8579b4ef292d227734b06c7b2', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-27 12:42:37.465000');
+INSERT INTO `goods_order` VALUES (26, 2, 5, 4, '黑', 'S', 1, '23af39773b9947e3b36b6146e932b25e', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-27 12:42:37.465000');
+INSERT INTO `goods_order` VALUES (27, 2, 5, 4, '黑', 'S', 1, '18d9c4958f634fb083ea805453b90abc', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-27 12:42:37.465000');
+INSERT INTO `goods_order` VALUES (28, 2, 5, 4, '黑', 'S', 1, 'fa51cdea56f34515aa2176e757d2d0ad', '//img.alicdn.com/bao/uploaded/i1/106802831/TB2bMLgtFXXXXc6XXXXXXXXXXXX_!!106802831.jpg_180x180q90.jpg_.webp', '冬宽松休闲外套大毛领连帽工装棉服男潮外套', 38, 'b@163.com', '2019-11-27 12:42:37.465000');
 
 -- ----------------------------
 -- Table structure for user
